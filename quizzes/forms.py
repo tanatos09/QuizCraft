8e463f7b249bc0be374f107a2224cca076048b99
tanatos09 <tanatos09@gmail.com>
2024-11-24 17:26:26 +0100

    starting make forms for quiz and questions

---
+++ quizzes/forms.py
@@ -0,0 +1,29 @@
+
+from django.forms import ModelForm
+from models import Quiz, Question, Answer
+
+class QuizForm(ModelForm):
+    class Meta:
+        model = Quiz
+        fields = '__all__'
+        labels = {
+            'title' : 'Title',
+            'description' : 'Description',
+        }
+
+class QuestionForm(ModelForm):
+    class Meta:
+        model = Question
+        fields = '__all__'
+        labels = {
+            'text' : 'Question text',
+        }
+
+class AnswerForm(ModelForm):
+    class Meta:
+        model = Answer
+        fields = '__all__'
+        labels = {
+            'text' : 'Answer text',
+            'is_correct' : 'Correct answer',
+        }
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,5 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from quizzes.forms import QuizForm, QuestionForm
+from quizzes.models import Quiz, Question
 
 
 def home(request):
     return render(request, "home.html")
+
+def create_quiz(request):
+    if request.method == "POST":
+        form = QuizForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quiz_list')
+
+    else: form = QuizForm()
+    return render(request, 'quizzes/create_quiz.html')
+
+def add_question(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.quiz = quiz
+            question.save()
+            return redirect('quiz_detail', quiz_id=quiz.id)
+    else:
+        form = QuestionForm(initial={'quiz_id': quiz.id})
+    return render(request, 'quizzes/add_question.html', {'form': form})
+
+
